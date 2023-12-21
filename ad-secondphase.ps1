5680f4b4febd30c476b67f76e0a6818401f32871
Cody Juhl <147422815+Skalyx866@users.noreply.github.com>
2023-12-20 23:20:04 -0800

    Add files via upload

---
+++ ad-secondphase.ps1
@@ -0,0 +1,3 @@
+﻿# simple script to add user, will expand upon later
+
+New-ADUser -Name "John Doe" -GivenName "John" -Surname "Doe" -SamAccountName "Jdoe" -UserPrincipalName "Jdoe@innovative.com" -Path "OU=Marketing,DC=innovative,DC=com" -Enabled $true
